3b6b2345eef27d233b409e98042419445ea3e356
Darragh Van Tichelen <info@darragh.dev>
2020-02-14 20:26:22 +0100

    Add logo!

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,73 +1,61 @@
 module.exports = {
-  siteName: 'PlanarAlly',
+  siteName: "PlanarAlly",
   templates: {
-    BlogPost: '/blog/:year/:month/:day/:title',
+    BlogPost: "/blog/:year/:month/:day/:title"
   },
   plugins: [
     {
-      use: '@gridsome/plugin-google-analytics',
+      use: "@gridsome/source-filesystem",
       options: {
-        id: 'UA-151236968-1'
-      }
-    },
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        path: './blog/*/index.md',
-        typeName: 'BlogPost',
+        path: "./blog/*/index.md",
+        typeName: "BlogPost",
         refs: {
-          author: 'Contributor'
+          author: "Contributor"
         }
       }
     },
     {
-      use: '@gridsome/vue-remark',
+      use: "@gridsome/vue-remark",
       options: {
-        index: ['README'],
-        baseDir: './docs',
-        pathPrefix: '/docs',
-        typeName: 'DocPage',
-        template: './src/templates/DocPage.vue',
-        plugins: [
-          '@gridsome/remark-prismjs'
-        ],
+        index: ["README"],
+        baseDir: "./docs",
+        pathPrefix: "/docs",
+        typeName: "DocPage",
+        template: "./src/templates/DocPage.vue",
+        plugins: ["@gridsome/remark-prismjs"],
         remark: {
           autolinkHeadings: {
             content: {
-              type: 'text',
-              value: '#'
+              type: "text",
+              value: "#"
             }
           }
         }
       }
     },
     {
-      use: '@gridsome/vue-remark',
+      use: "@gridsome/vue-remark",
       options: {
-        index: ['README'],
-        baseDir: './tutorial',
-        pathPrefix: '/tutorial',
-        typeName: 'TutorialPage',
-        template: './src/templates/TutorialPage.vue',
-        plugins: [
-          '@gridsome/remark-prismjs'
-        ],
+        index: ["README"],
+        baseDir: "./tutorial",
+        pathPrefix: "/tutorial",
+        typeName: "TutorialPage",
+        template: "./src/templates/TutorialPage.vue",
+        plugins: ["@gridsome/remark-prismjs"],
         remark: {
           autolinkHeadings: {
             content: {
-              type: 'text',
-              value: '#'
+              type: "text",
+              value: "#"
             }
           }
         }
       }
-    },
+    }
   ],
   chainWebpack: config => {
-    const svgRule = config.module.rule('svg')
-    svgRule.uses.clear()
-    svgRule
-      .use('vue-svg-loader')
-      .loader('vue-svg-loader')
+    const svgRule = config.module.rule("svg");
+    svgRule.uses.clear();
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
   }
-}
+};
---
+++ src/layouts/partials/Logo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="logo">
+    <g-link class="logo__link" to="/" title="Back to home">
+      <!-- <LogoSmall class="logo__svg hide-for-small" alt="Gridsome.org" /> -->
+      <LogoSmall class="logo__svg logo__svg--small" alt="Gridsome.org" />
+    </g-link>
+  </div>
+</template>
+
+<script>
+import LogoSmall from "~/assets/images/pa-logo-small.svg";
+export default {
+  components: {
+    LogoSmall
+  }
+};
+</script>
+
+<style lang="scss">
+.logo {
+  &__svg {
+    max-width: 60px;
+    height: auto;
+    margin-top: 3px;
+    margin-bottom: 5px;
+    &--small {
+      max-width: 50px;
+    }
+  }
+}
+</style>
