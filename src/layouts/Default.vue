bc6b7b7d65a4656b9e79b5744cb99778247084b6
Darragh Van Tichelen <info@darragh.dev>
2021-02-07 18:45:29 +0100

    Upgrade final dependencies

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,34 +1,34 @@
 <template>
-    <div id="app" dark>
-      <Header />
-      <main id="main" :class="mainClass">
-        <slot />
-      </main>
-      <LazyHydrate ssr-only v-if="footer !== false">
-        <Footer />
-      </LazyHydrate>
-    </div>
+  <div id="app" dark>
+    <Header />
+    <main id="main" :class="mainClass">
+      <slot />
+    </main>
+    <LazyHydrate never v-if="footer !== false">
+      <Footer />
+    </LazyHydrate>
+  </div>
 </template>
 
 <script>
-import Header from './partials/Header'
-import Footer from './partials/Footer'
-import LazyHydrate from 'vue-lazy-hydration'
+import Header from "./partials/Header";
+import Footer from "./partials/Footer";
+import LazyHydrate from "vue-lazy-hydration";
 export default {
-  props: ['footer', 'primary-bg'],
+  props: ["footer", "primary-bg"],
   components: {
     Header,
     Footer,
-    LazyHydrate
+    LazyHydrate,
   },
   computed: {
     mainClass() {
-      let classes = []
-      if(this.primaryBg) classes.push('main--bg-teritary')
-      return classes
+      let classes = [];
+      if (this.primaryBg) classes.push("main--bg-teritary");
+      return classes;
     },
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss">
