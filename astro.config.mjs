536faeade0664f2a2f16d21c8312768266eda582
Filippo Di Marco <filidm5795@gmail.com>
2026-08-20 01:08:07 +0200

    fix(astro.config.mjs): fix redirects indentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import Icons from "unplugin-icons/vite";
 
 // https://astro.build/config
 export default defineConfig({
-redirects: {
+    redirects: {
         "/server": "/server/setup/",
     },
     integrations: [
